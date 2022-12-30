62d8723c762b0653b6369a81969b36cc523ec588
siddhilb <79735001+siddhilb@users.noreply.github.com>
2022-12-30 12:03:50 -0800

    added gui

--- chat.py
+++ chat.py
@@ -4,9 +4,10 @@ import random
 import tkinter as tk
 from tkinter import *
 root = tk.Tk()
+root.geometry("700x700")
 key ="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"
 openai.api_key = key
-generate = tk.Button(text="Generate random excuse",font=('Arial',20))
+
 def make_excuse():
     temp = random.uniform(0.9,1.5)
     seed = random.randint(0,1000)
@@ -14,5 +15,12 @@ def make_excuse():
     choices=response["choices"]
     choices_dict=choices[0]
     return choices_dict["text"]
-make_excuse()
+excuse = tk.Label(font=('Arial',30),text="",wraplength=400,justify=CENTER)
+
+def command():
+    excuse.config(text=make_excuse())
+    excuse.pack()
+generate = tk.Button(text="Generate random excuse",font=('Arial',20),command=command)
+generate.place(anchor=S,relx=0.5,rely=0.8)
+
 root.mainloop()
