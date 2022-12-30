df75468874962e9d4f9de8c8a95d8d1c3c3a3cbc
siddhilb <79735001+siddhilb@users.noreply.github.com>
2022-12-30 12:51:21 -0800

    made it prettty

--- chat.py
+++ chat.py
@@ -1,26 +1,38 @@
 import openai
 from tkinter import ttk
 from ttkthemes import ThemedTk
+from PIL import ImageTk, Image
+
 import os
 import random
-root = ThemedTk(theme="yaru")
-root.geometry("700x700")
+root = ThemedTk(theme="adapta")
+root.geometry("700x400")
 key ="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"
 openai.api_key = key
+what = ttk.Entry()
+photo = ImageTk.PhotoImage(file = "icon.png")
+root.iconphoto(False, photo)
 
-def make_excuse():
+root.title("Random Excuse Generator")
+def make_excuse(who):
     temp = random.uniform(0.9,1.5)
     seed = random.randint(0,1000)
-    response = openai.Completion.create(model="text-davinci-003", prompt=f"Using the seed {seed}, generate an excuse without mentioning the seed;", temperature=temp, max_tokens=256)
+    prompt=f"Using the seed {seed}, generate an excuse assuming you are {who} without mentioning the seed;"
+    response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=temp, max_tokens=256)
     choices=response["choices"]
     choices_dict=choices[0]
     return choices_dict["text"]
+
 excuse = ttk.Label(text="",wraplength=400,justify="center")
 
 def command():
-    excuse.config(text=make_excuse())
-    excuse.pack()
+    who=what.get()
+    excuse.config(text=make_excuse(who))
+    excuse.place(anchor="center",relx=0.5,rely=0.3)
 generate = ttk.Button(text="Generate random excuse",command=command)
-generate.place(anchor="center",relx=0.5,rely=0.8)
+generate.place(anchor="center",relx=0.5,rely=0.7)
+ttk.Label(text="Who are you? Enter below.").place(anchor="center",relx=0.5,rely=0.85)
+what.place(anchor="center",relx=0.5,rely=0.95)
+root.configure(background='white')
 
 root.mainloop()
