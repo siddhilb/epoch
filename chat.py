021080905a4fa05445c7da710448436a14818a72
siddhilb <79735001+siddhilb@users.noreply.github.com>
2022-12-30 11:45:43 -0800

    made it work

--- chat.py
+++ chat.py
@@ -1,9 +1,18 @@
 import openai
 import os
+import random
+import tkinter as tk
+from tkinter import *
+root = tk.Tk()
 key ="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"
 openai.api_key = key
-
-response = openai.Completion.create(model="text-davinci-003", prompt="Generate an excuse", temperature=1.5, max_tokens=50)
-choices=response["choices"]
-choices_dict=choices[0]
-print(choices_dict["text"])
+generate = tk.Button(text="Generate random excuse",font=('Arial',20))
+def make_excuse():
+    temp = random.uniform(0.9,1.5)
+    seed = random.randint(0,1000)
+    response = openai.Completion.create(model="text-davinci-003", prompt=f"Using the seed {seed}, generate an excuse without mentioning the seed;", temperature=temp, max_tokens=256)
+    choices=response["choices"]
+    choices_dict=choices[0]
+    return choices_dict["text"]
+make_excuse()
+root.mainloop()
--- main.py
+++ main.py
@@ -2,14 +2,7 @@ import tkinter as tk
 from tkinter import *
 import requests
 import json
-import openai
-openai.api_key = "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"
 
-openai.Completion.create(
-  engine="davinci",
-  prompt="Make a list of astronomical observatories:"
-)
-print(prompt)
 root = tk.Tk()
 root.geometry("600x600")
 root.mainloop()
