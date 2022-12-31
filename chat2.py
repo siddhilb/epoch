0d6b6f1b284b525145077fc160c6b496e723068a
siddhilb <79735001+siddhilb@users.noreply.github.com>
2022-12-30 17:02:27 -0800

    finishing touches #2

--- chat2.py
+++ chat2.py
@@ -7,11 +7,11 @@ import os
 import random
 root = ThemedTk(theme="adapta")
 root.geometry("700x400")
-key ="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"
+key ="your key here"
 openai.api_key = key
 what = ttk.Entry()
 photo = ImageTk.PhotoImage(file = "icon.png")
-noun = openai.Completion.create(model="text-davinci-003", prompt="Generate a noun of a living thing that needs an excuse, including the reason it needs one:", temperature=0.8, max_tokens=10)
+noun = openai.Completion.create(model="text-davinci-003", prompt="Generate a human noun with an occupation without punctuation:", temperature=0.8, max_tokens=10)
 noun2 = noun["choices"]
 nounreal=noun2[0]
 root.iconphoto(False, photo)
@@ -31,10 +31,10 @@ what.bind("<Button-1>", mouseclick)
 
 
 root.title("Random Excuse Generator")
-def make_excuse(who,what2):
+def make_excuse(who):
     temp = random.uniform(0.9,1.2)
     seed = random.randint(0,1000)
-    prompt=f"Using the seed {seed}, generate an excuse using {what}, assuming you are {who} without mentioning the seed;"
+    prompt=f"Using the seed {seed}, generate an excuse assuming you are {who} without mentioning the seed;"
     response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=temp, max_tokens=256)
     choices=response["choices"]
     choices_dict=choices[0]
@@ -44,12 +44,11 @@ excuse = ttk.Label(text="",wraplength=400,justify="center")
 
 def command():
     global autotext
-    who = what.get()
-    what2=what.get()
+    who=what.get()
     if autotext:
         what.delete(0,"end")
     if what.get().strip() == "":
-        noun = openai.Completion.create(model="text-davinci-003", prompt="Generate a noun of a living thing that needs an excuse, including the reason it needs one:" ,temperature=0.8, max_tokens=10)
+        noun = openai.Completion.create(model="text-davinci-003", prompt="Generate a noun of a living thing without punctuation:", temperature=0.8, max_tokens=10)
         noun2 = noun["choices"]
         nounreal=noun2[0]
         what.insert(0, nounreal["text"].strip())
@@ -57,13 +56,12 @@ def command():
         what.config(foreground="gray")
         who=what.get()
 
-    excuse.config(text=make_excuse(who,what2))
+    excuse.config(text=make_excuse(who))
     excuse.place(anchor="center",relx=0.5,rely=0.3)
-ttk.Label(text="Who are you? What did you do? Enter below.").place(anchor="center",relx=0.5,rely=0.65)
 generate = ttk.Button(text="Generate random excuse",command=command)
-generate.place(anchor="center",relx=0.5,rely=0.92)
-
-what.place(anchor="center",relx=0.5,rely=0.75)
+generate.place(anchor="center",relx=0.5,rely=0.7)
+ttk.Label(text="Who are you? Enter below.").place(anchor="center",relx=0.5,rely=0.85)
+what.place(anchor="center",relx=0.5,rely=0.95)
 root.configure(background='white')
 
 root.mainloop()
